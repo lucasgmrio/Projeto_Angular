67fbc81c5bfcf7a67c0dd404a58a3779c32638e1
Lucas Santos <lucasgmrio@gmail.com>
2023-08-25 21:04:08 -0300

    Criado serviço de ofertas a nivel de component , criado modelo oferta

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[OfertasService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+public ofertas : Oferta[] = [];
+
+  constructor(private ofertaService: OfertasService){
+  }
+
+  ngOnInit(){
+
+   this.ofertas = this.ofertaService.getOfertas()
+
+   console.log(this.ofertas)
+  }
 
 }
